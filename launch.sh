7d1fbb08108f80e07804c8adda2fc43d428a2c95
Lilian Besson <lilian@besson.link>
2020-11-17 15:41:06 +0100

    OK for generating H+0,H+1,H+2,H+3

--- launch.sh
+++ launch.sh
@@ -9,28 +9,33 @@ ls -larth *.pdf
 
 dateNow="$(date '+%Y-%m-%d')"
 #namePDF=$(echo attestation-${dateNow}_*.pdf)
-namePDF="$(ls -larth attestation-${dateNow}*pdf | tail -n4 | grep -o 'attestation.*.pdf')"
 
-if [ ! -f "$namePDF" ]; then
-	exit 1;
-fi
+# TODO customize this -4 from a cli argument?
+namesPDF="$(ls -larth attestation-${dateNow}*pdf | tail -n4 | grep -o 'attestation.*.pdf')"
 
-# Generate a PNG image from the PDF
-# http://askubuntu.com/questions/50170/ddg#50180
-namePNG="${namePDF%.pdf}"
-pdftoppm "${namePDF}" "${namePNG}" -png -f 1 -singlefile
-namePNG="${namePNG}.png"
+for namePDF in $namesPDF; do
+    if [ ! -f "$namePDF" ]; then
+        exit 1;
+    fi
 
-# now compress both PDF and PNG
-PDFCompress "${namePDF}"
-advpng -z -2 "${namePNG}"
+    # Generate a PNG image from the PDF
+    # http://askubuntu.com/questions/50170/ddg#50180
+    namePNG="${namePDF%.pdf}"
+    pdftoppm "${namePDF}" "${namePNG}" -png -f 1 -singlefile
+    namePNG="${namePNG}.png"
 
-echo CP "${namePDF}" "${namePNG}" "${Szam}attestations/${dateNow}/"
-CP "${namePDF}" "${namePNG}" "${Szam}attestations/${dateNow}/"
+    # now compress both PDF and PNG
+    PDFCompress "${namePDF}"
+    advpng -z -2 "${namePNG}"
 
-echo "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
-notify-send "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
-echo FreeSMS.py "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
-FreeSMS.py "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
+    echo CP "${namePDF}" "${namePNG}" "${Szam}attestations/${dateNow}/"
+    CP "${namePDF}" "${namePNG}" "${Szam}attestations/${dateNow}/"
+
+    echo "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
+    notify-send "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
+    echo FreeSMS.py "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
+    FreeSMS.py "Une nouvelle attestation vient d'être générée à ${dateNow}, elle est désormais disponible ici : https://perso.crans.org/besson/attestations/${dateNow}/${namePDF} et https://perso.crans.org/besson/attestations/${dateNow}/${namePNG}"
+
+done
 
 # make clean
